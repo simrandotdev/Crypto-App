ea657525da19ecd8ec3bdcd96f228edd19064875
Simran Preet Singh <simran5590@gmail.com>
2022-10-10 11:38:59 -0700

    Custom header in HomeView

---
+++ App/Core/Home/Views/HomeView.swift
@@ -0,0 +1,42 @@
+//
+//  HomeView.swift
+//  Crypto App
+//
+//  Created by Simran Preet Narang on 2022-10-10.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        ZStack {
+            Color.theme.background
+                .ignoresSafeArea()
+            
+            VStack {
+                HStack {
+                    CircleButtonView(iconName: "info")
+                    Spacer()
+                    Text("Live Prices")
+                        .font(.headline)
+                        .fontWeight(.heavy)
+                        .foregroundColor(Color.theme.accent)
+                    Spacer()
+                    CircleButtonView(iconName: "chevron.right")
+                }
+                .padding()
+                
+                Spacer()
+            }
+        }
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationView {
+            HomeView()
+                .toolbar(.hidden)
+        }
+    }
+}
